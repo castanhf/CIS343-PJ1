0a0427f1bf54a7d8ca18db6ee897d80667afab35
Filipe Castanheira <castanhf@eos07.cis.gvsu.edu>
2018-09-24 01:31:15 -0400

    Final Update! Project is complete and fulfills all of its requirements

--- file_utils.c
+++ file_utils.c
@@ -1,53 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-//#include <errno-base.h>
 #include <sys/stat.h>
 #include "file_utils.h"
 
 int read_file( char* filename, char **buffer ){
 	
+	//File pointer
 	FILE *input;
 	
-	//gets size of the file
+	//The following three lines of code get
+	//get the size of the file
 	//(only works in Linux)
 	struct stat st;
 	stat(filename, &st);
 	int size = st.st_size;
 
+	//memory allocation from the heap in order
+	//to read a file
 	*buffer = (char*)malloc(size * sizeof(char));
-
-	input = fopen(filename, "r");
-
-	if(input == NULL) {
-		fprintf(stderr, "File %s has not opened to read.", filename);
-		exit(-1);
+	if(*buffer == NULL) {
+		fprintf(stderr, "Insufficient memory available! Make a memory allocation!\n");
+		exit(EXIT_FAILURE);
 	}
 
+
+	//pointer input is assigned with a file,
+	//which is opened in read mode
+	input = fopen(filename, "r");
+
+	//Exception handling
+	//Checks if file was opened correctly
+	//Otherwise report error
+	if(input == NULL) {
+		fprintf(stderr, "File %s has not opened to read.\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	//Process of reading the file
 	fgets(*buffer, size, input);
 
+
+	//Closes the file
 	fclose(input);	
 	return size;
 }
 
-int write_file( char* filename, char *buffer, int size ){
+int write_file( char *filename, char *buffer, int size ){
 	
-
+	//File pointer
 	FILE *output;
        
+	//pointer output is assigned a file,
+	//which is opened to write
+	//(if file does not exist, then it will create
+	//a new one)
 	output = fopen(filename, "w");
 
+	//Exception handling
+	//Checks if file was opened correctly
 	if(output == NULL) {
-		fprintf(stderr, "File %s has not opened to write.", filename);
-		exit(-1);
+		fprintf(stderr, "File %s has not opened to write.\n", filename);
+		exit(EXIT_FAILURE);
 	}
 
-	int i;
-	for(i = 0; i < size ; i++){
-		fprintf(output, "%c", buffer[i]);
-		printf("%c", buffer[i]);
-	}
+	//Process of writing the file
+	fwrite(buffer, 1, size, output);
 
+	//Closes the file
 	fclose(output);
 	return 0;
 }
--- reverse.c
+++ reverse.c
@@ -5,31 +5,56 @@
 
 int main(int argc, char* argv[]){
 
+	//variable of type int
 	int numByte;
+
+	//char pointers
 	char *readDoc;
 	char *writeDoc;
 	char *writeBuffer;
+
+	//pointer the a (char) pointer
 	char **buf;
 	
+	//This char pointer stores the first argument
+	//in the command line after the executable
 	readDoc = argv[1];
 
+	//This char pointer stores the second argument
+	//in the command line after the executable
 	writeDoc = argv[2];
 
+	//pointer to a (char) pointer stores the
+	//memory location of readDoc, which is a
+	//char pointer
 	buf = &readDoc;
 
+	//numByte stores the size of a file after
+	//the execution of read_file method
 	numByte = read_file(readDoc, buf);
 
+	//Memory allocation
+	//writeBuffer gets borrows memoery from heap
+	//for future file writings
 	writeBuffer = (char*)malloc(numByte * sizeof(char*));
+	if(writeBuffer == NULL) {
+		fprintf(stderr, "Insufficient memory! Make a memory allocation!\n");
+		exit(EXIT_FAILURE);
+	}	
 
-	//reverse happens here
+	//Reverse happens here
+	//Reversed array of chars are stored
+	//in char pointer writeBuffer
 	for(int i = 0; readDoc[i] != '\0'; i++) {
-	//	fprintf(writeDoc, "%c", buf[numByte - i - 1]);
 		writeBuffer[i] = readDoc[numByte - i - 2];
 	}
 	
-
+	//The writing of the reversed array of chars
+	//into a file
 	write_file(writeDoc, writeBuffer, numByte);
 	
+	//Maintenance of the borrowed memory
+	//from the heap
 	free(readDoc);
 	free(writeBuffer);
 	
